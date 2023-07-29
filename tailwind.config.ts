1d473e249749b04de77d9fcf9cafd8d3a3d60f04
HARI-PRMD <haripramodb@gmail.com>
2023-07-29 23:26:22 +1000

    added media breaks

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,14 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      xs: "320px", // iPhone SE breakpoint
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     extend: {},
   },
   plugins: [],
