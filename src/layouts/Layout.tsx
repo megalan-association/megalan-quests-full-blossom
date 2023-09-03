42c4ee14d1e87000135059eecec0489727391344
max8539 <maxyuen8539@gmail.com>
2023-09-03 22:19:29 +1000

    Navbar improvements

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -6,7 +6,8 @@ interface Props {
 }
 
 const Layout: React.FC<Props> = ({ children }) => {
-  return (
+  return (<>
+    <div className="absolute h-full w-full -z-50 left-0 top-o bg-beige"></div>
     <main className="flex h-full w-full flex-col justify-between bg-beige text-black">
       <div className="">
         <NavBar />
@@ -15,7 +16,7 @@ const Layout: React.FC<Props> = ({ children }) => {
       <div>Footer Here</div>
       {/* <Footer /> */}
     </main>
-  );
+    </>);
 };
 
 export default Layout;
--- src/pages/allTasks.tsx
+++ src/pages/allTasks.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from "next";
 import TaskCard from "~/components/TaskCard";
+import Layout from "~/layouts/Layout";
 
 const AllTasksPage: NextPage = () => {
 
@@ -20,9 +21,9 @@ const AllTasksPage: NextPage = () => {
   return (
     <>
     {/* {allTasks.map(t => {return <TaskCard taskId={t.id}/>})} */}
-
-    <TaskCard taskId={"T1234"} title={"Test Task"} socName={"BestSoc"} desc={"super hard challenge "} diff={"Hard"} compStat={false} points={1000} />
-    
+    <Layout>
+      <TaskCard taskId={"T1234"} title={"Test Task"} socName={"BestSoc"} desc={"super hard challenge "} diff={"Hard"} compStat={false} points={1000} />
+    </Layout>
     </>
   );
 
