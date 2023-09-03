dc0115a592d6d2e0a7329239164535c515a87b62
max8539 <maxyuen8539@gmail.com>
2023-09-03 22:44:34 +1000

    New navbar changes

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -6,8 +6,9 @@ interface Props {
 }
 
 const Layout: React.FC<Props> = ({ children }) => {
-  return (
-    <main className="flex h-full w-full flex-col justify-between bg-beige text-black">
+  return (<>
+    <div className="absolute top-o left-0 w-full h-full -z-50 bg-beige"></div>
+    <main className="flex h-full w-full flex-col justify-between text-black">
       <div className="">
         <NavBar />
         <div className="pt-24">{children}</div>
@@ -15,7 +16,7 @@ const Layout: React.FC<Props> = ({ children }) => {
       <div>Footer Here</div>
       {/* <Footer /> */}
     </main>
-  );
+  </>);
 };
 
 export default Layout;
--- src/layouts/NavBar.tsx
+++ src/layouts/NavBar.tsx
@@ -33,12 +33,12 @@ const NavBar = () => {
             className="h-16 w-fit object-contain invert"
           />
         </Link>
-        <div className="hidden h-full flex-row space-x-24 py-2 focus:outline-none md:flex">
+        <div className="hidden h-full flex-row sm:space-x-3 lg:space-x-12 xl:space-x-24 py-2 focus:outline-none md:flex">
           {links.map((link) => (
             <Link
               href={link.href}
               key={link.href}
-              className="group p-2 font-body text-3xl"
+              className="group p-2 font-body text-2xl"
             >
               {link.name}
               <span className="block h-0.5 w-0 transform bg-brown transition-all duration-300 group-hover:w-full" />
@@ -46,7 +46,7 @@ const NavBar = () => {
           ))}
         </div>
         <button className="bg-primary-red hidden h-fit border border-brown p-4 drop-shadow-lg md:block">
-          <p className="font-body text-xl">Login / Sign Up</p>
+          <p className="font-body text-xl">Login</p>
         </button>
       </div>
       <Menu>
