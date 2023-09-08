08fcaf9c3a736083ab68bc71f8c3b519982e3e8b
HARI-PRMD <haripramodb@gmail.com>
2023-09-08 21:08:43 +1000

    Created Back Button

---
+++ src/components/BackButton.tsx
@@ -0,0 +1,20 @@
+import { ArrowLeftIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
+
+interface Props {
+  href: string;
+  text: string;
+}
+const BackButton: React.FC<Props> = ({ href, text }) => {
+  return (
+    <Link
+      href={href}
+      className="flex h-fit w-fit flex-row items-center space-x-2 rounded-2xl bg-pink/20 px-4 py-2 text-pink"
+    >
+      <ArrowLeftIcon className="h-4 w-4 md:h-6 md:w-6" />
+      <p className="font-heading text-xs font-medium md:text-base">{text}</p>
+    </Link>
+  );
+};
+
+export default BackButton;
