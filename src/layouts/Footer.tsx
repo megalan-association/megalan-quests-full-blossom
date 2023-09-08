1988a8db70f71aed067a2bd0bc1b7e55ac55a16a
HARI-PRMD <haripramodb@gmail.com>
2023-09-08 21:08:07 +1000

    Created Footer with BreadCrumbs

---
+++ src/layouts/Footer.tsx
@@ -0,0 +1,13 @@
+import { useRouter } from "next/router";
+
+const Footer = () => {
+  const router = useRouter();
+  return (
+    <div className="h-fit w-full p-4">
+      <p className="font-body text-base font-semibold text-brown md:text-xl">
+        {router.pathname.replaceAll("/", " / ")}
+      </p>
+    </div>
+  );
+};
+export default Footer;
--- src/layouts/UserPageLayout.tsx
+++ src/layouts/UserPageLayout.tsx
@@ -1,25 +1,36 @@
+import BackButton from "~/components/BackButton";
 import Header from "./Header";
 import NavBar from "./NavBar";
+import Footer from "./Footer";
 
 interface Props {
   children: React.ReactNode;
   headingText: string;
+  backHref?: string;
+  backText?: string;
 }
 
-const UserPageLayout: React.FC<Props> = ({ children, headingText }) => {
+const UserPageLayout: React.FC<Props> = ({
+  children,
+  headingText,
+  backHref,
+  backText,
+}) => {
   return (
     <main className="flex h-full min-h-screen w-full flex-col justify-between bg-beige text-black">
       <div className="">
         <NavBar />
         <div className="flex flex-row justify-center pt-24">
           <div className="md:container">
+            {backHref && backText && (
+              <BackButton href={backHref} text={backText} />
+            )}
             <Header headingText={headingText} />
             {children}
           </div>
         </div>
       </div>
-      <div>Footer Here</div>
-      {/* <Footer /> */}
+      <Footer />
     </main>
   );
 };
