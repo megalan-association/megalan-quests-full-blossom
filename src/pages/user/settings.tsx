adbdff89d0185fd20c334fb1fba982c6fd17b5ac
HARI-PRMD <haripramodb@gmail.com>
2023-09-10 19:58:38 +1000

    Completed Settings Page

--- src/pages/user/settings.tsx
+++ src/pages/user/settings.tsx
@@ -7,68 +7,89 @@ import UserPageLayout from "~/layouts/UserPageLayout";
 
 const Settings = () => {
   const { data: session } = useSession();
-  const [editName, setEditName] = useState(false);
+  const [adminToken, setAdminToken] = useState("");
   const [newName, setNewName] = useState("");
-  const [error, setError] = useState(false);
+  const [nameError, setNameError] = useState(false);
+  const [tokenError, setTokenError] = useState(false);
+  const [tokenSuccess, setTokenSuccess] = useState(false);
+  const [nameSuccess, setNameSuccess] = useState(false);
 
   if (!session?.user) return <NotLoggedIn />;
+
   const handleNameChangeSubmit = (e: React.SyntheticEvent) => {
     e.preventDefault();
-    setEditName(false);
     // api call here
+    setNewName("");
+    setNameSuccess(true);
+    setTimeout(() => {
+      setNameSuccess(false);
+    }, 2500);
   };
+
+  const handleAdminTokenSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    // api call here
+    setAdminToken("");
+    setTokenSuccess(true);
+    setTimeout(() => {
+      setTokenSuccess(false);
+    }, 2500);
+  };
+
   return (
     <UserPageLayout
       headingText="Settings"
       backHref="/user/dashboard"
       backText="Back to User Dashboard"
     >
-      <section className="h-full w-full space-y-16 py-8">
+      <section className="h-full w-full space-y-8 py-8">
         <ProfileCard />
-        <div className="flex w-full flex-col items-center justify-center space-y-8">
-          <div className="flex flex-col items-center">
-            <button
-              onClick={() => setEditName(!editName)}
-              className="flex w-full items-center justify-between space-x-4 rounded-xl bg-gradient-to-br from-green/20 to-green/40 px-4 py-2 "
-            >
-              <p className="font-xl font-heading text-brown">Change Name</p>
+        <div className="flex w-full flex-col items-center justify-center space-y-4">
+          <div className="flex w-full flex-col items-center">
+            <div className="flex w-full items-center justify-between space-x-4 px-2">
+              <p className="font-heading text-xl font-medium text-brown">
+                Change Name
+              </p>
               <PencilIcon className="h-6 w-6 text-brown" />
-            </button>
-            {editName && (
-              <form
-                onSubmit={handleNameChangeSubmit}
-                className="space-y-4 overflow-clip pt-2"
-              >
-                <div className="space-y-2">
-                  <label
-                    htmlFor="username"
-                    className="font-heading text-base text-brown"
-                  >
-                    New Username
-                  </label>
-                  <input
-                    id="username"
-                    type="text"
-                    className={`w-full rounded-xl p-4 ${
-                      error
-                        ? "border-4 border-red-500"
-                        : "border-4 border-green"
-                    } `}
-                    placeholder="username here"
-                    onChange={(e) => setNewName(e.currentTarget.value)}
-                  />
-                  {error && (
-                    <p className="px-4 font-body text-xs font-medium text-red-500">
-                      *{newName} is already taken
-                    </p>
-                  )}
-                </div>
+            </div>
 
+            <form
+              onSubmit={handleNameChangeSubmit}
+              className="space-y-4 overflow-clip pt-2"
+            >
+              <div className="space-y-2">
+                <label
+                  htmlFor="username"
+                  className="font-heading text-base text-brown"
+                >
+                  New Username
+                </label>
+                <input
+                  id="username"
+                  type="text"
+                  value={newName}
+                  className={`w-full rounded-xl p-4 ${
+                    nameError
+                      ? "border-4 border-red-500"
+                      : "border-4 border-green"
+                  } `}
+                  placeholder="username"
+                  onChange={(e) => setNewName(e.currentTarget.value)}
+                />
+                {nameError && (
+                  <p className="px-4 font-body text-xs font-medium text-red-500">
+                    *{newName} is already taken
+                  </p>
+                )}
+                {nameSuccess && (
+                  <p className="px-4 font-body text-xs font-medium text-green">
+                    Your New Username is Valid !
+                  </p>
+                )}
+              </div>
+              {newName.length > 0 && (
                 <div className="flex w-full flex-row justify-end space-x-2">
-                  <button
-                    onClick={() => setEditName(false)}
-                    className="rounded-xl bg-red-500/20 px-8 py-2 text-red-500"
-                  >
+                  <button className="rounded-xl bg-red-500/20 px-8 py-2 text-red-500">
                     Cancel
                   </button>
                   <button
@@ -78,15 +99,67 @@ const Settings = () => {
                     Save
                   </button>
                 </div>
-              </form>
-            )}
+              )}
+            </form>
           </div>
-          <button className="font-xl flex w-fit items-center rounded-xl bg-gradient-to-br from-green/20 to-green/40 px-4 py-2 font-heading text-brown">
-            Become Admin
-            <span className="inline-block pl-2">
+
+          <div className="flex w-full flex-col items-center px-2">
+            <div className="flex w-full items-center justify-between space-x-4">
+              <p className="font-heading text-xl font-medium text-brown">
+                Become an Admin
+              </p>
               <UsersIcon className="h-6 w-6 text-brown" />
-            </span>
-          </button>
+            </div>
+            <form
+              id="admin-token-form"
+              onSubmit={handleAdminTokenSubmit}
+              className="space-y-4 "
+            >
+              <div className="space-y-2">
+                <label
+                  htmlFor="username"
+                  className="font-heading text-base text-brown"
+                >
+                  Add Society Token
+                </label>
+                <input
+                  id="admin-token"
+                  type="text"
+                  value={adminToken}
+                  className={`w-full rounded-xl p-4 ${
+                    tokenError
+                      ? "border-4 border-red-500"
+                      : "border-4 border-green"
+                  } `}
+                  placeholder="Society Token"
+                  onChange={(e) => setAdminToken(e.currentTarget.value)}
+                />
+                {tokenError && (
+                  <p className="px-4 font-body text-xs font-medium text-red-500">
+                    *The token you entered is invalid
+                  </p>
+                )}
+                {tokenSuccess && (
+                  <p className="px-4 font-body text-xs font-medium text-green">
+                    Your Token is Valid !
+                  </p>
+                )}
+              </div>
+              {adminToken.length > 0 && (
+                <div className="flex w-full flex-row justify-end space-x-2">
+                  <button className="rounded-xl bg-red-500/20 px-8 py-2 text-red-500">
+                    Cancel
+                  </button>
+                  <button
+                    type="submit"
+                    className="rounded-xl bg-green/20 px-8 py-2 text-green"
+                  >
+                    Add
+                  </button>
+                </div>
+              )}
+            </form>
+          </div>
         </div>
       </section>
     </UserPageLayout>
