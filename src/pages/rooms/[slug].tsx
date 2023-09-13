fd3bdc156477d4ce286846290fb1aeded7e92d80
max8539 <maxyuen8539@gmail.com>
2023-09-13 20:11:20 +1000

    Update filter dropdowns

--- src/pages/rooms/[slug].tsx
+++ src/pages/rooms/[slug].tsx
@@ -2,6 +2,7 @@ import { motion } from "framer-motion";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import TaskCard from "~/components/TaskCard";
+import ListInput from "~/components/input/ListInput";
 import UserPageLayout from "~/layouts/UserPageLayout";
 import { springTransition } from "~/utils/animations";
 import { api } from "~/utils/api";
@@ -59,39 +60,25 @@ const Room = () => {
      <UserPageLayout headingText="Quests">
       <div className="m-auto w-full font-heading font-bold sm:w-4/5 md:w-[640px]">
         <p className="text-[#F38DB4]">Filter By:</p>
-        <div className="flex space-x-2 pt-2 md:space-x-8">
-          <select
-            defaultValue="_"
-            onChange={(e) => {
-              filterDifficulty = e.target.value
+        <div className="grid grid-cols-2 space-x-2">
+          <ListInput 
+            id="difficulty-filter"
+            options={["All difficulties", "Easy", "Medium", "Hard"]}
+            selectedOption="All difficulties"
+            onSelect={(v) => {
+              filterDifficulty = v.toString()
+              updateFilter()
+            }} 
+          />
+          <ListInput 
+            id="society-filter"
+            options={["All societies", ...societies]}
+            selectedOption="All societies"
+            onSelect={(v) => {
+              filterSociety = v.toString()
               updateFilter()
             }}
-            className="block w-full appearance-none rounded-full border-4 border-[#F38DB4] bg-[#FFE5E5] p-3 text-center text-[#F38DB4]"
-          >
-            <option value="_" disabled>
-              Task difficulty
-            </option>
-            <option value="">All difficulties</option>
-            <option value="Easy">Easy</option>
-            <option value="Medium">Medium</option>
-            <option value="Hard">Hard</option>
-          </select>
-          <select
-            defaultValue="_"
-            onChange={(e) => {
-              filterSociety = e.target.value
-              updateFilter()
-            }}
-            className="block w-full appearance-none rounded-full border-4 border-[#F38DB4] bg-[#FFE5E5] p-3 text-center text-[#F38DB4]"
-          >
-            <option value="_" disabled>
-              Society
-            </option>
-            <option value="">All societies</option>
-            {societies.map((s, i) => {
-              return (<option key={i} value={s}>{s}</option>)
-            })}
-          </select>
+          />
         </div>
       </div>
       <div className="grid h-full w-full grid-cols-1 gap-8 py-8 md:grid-cols-3">
