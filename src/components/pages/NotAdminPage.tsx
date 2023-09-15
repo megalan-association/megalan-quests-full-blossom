d968b227962ed7ef163b227b99555b73c5973d93
HARI-PRMD <haripramodb@gmail.com>
2023-09-15 13:24:55 +1000

    Updated Warning / Loading pages

---
+++ src/components/pages/NotAdminPage.tsx
@@ -0,0 +1,34 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import Layout from "~/layouts/Layout";
+
+const NotAdminPage = () => {
+  const [seconds, setSeconds] = useState(5);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (seconds === 0) {
+      router.push("/user/dashboard");
+    }
+    setTimeout(() => {
+      setSeconds(seconds - 1);
+    }, 1000);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [seconds]);
+
+  return (
+    <Layout>
+      <div className="flex h-[60dvh] w-full flex-col items-center justify-center px-4">
+        <div className="flex w-full flex-col items-center justify-center">
+          <div className="font-body text-2xl font-semibold text-brown md:text-5xl">
+            You are Not an Admin
+          </div>
+          <div className="font-body text-lg font-medium text-brown md:text-2xl">
+            Going back to Participant Dashboard in {seconds} Seconds
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+export default NotAdminPage;
